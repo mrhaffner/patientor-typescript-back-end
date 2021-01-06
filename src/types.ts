a736ffd8065e3f9fd6f5f938dcecd350ee359f17
Matt Haffner <mrhaffner13@gmail.com>
2021-01-06 16:29:50 -0600

    added most of the type checking for new entries to existing patients

--- src/types.ts
+++ src/types.ts
@@ -10,7 +10,7 @@ export enum Gender {
     Other = 'other',
 }
 
-interface BaseEntry {
+export interface BaseEntry {
     id: string;
     description: string;
     date: string;
@@ -25,7 +25,7 @@ export enum HealthCheckRating {
     "CriticalRisk" = 3
 }
   
-interface HealthCheckEntry extends BaseEntry {
+export interface HealthCheckEntry extends BaseEntry {
     type: "HealthCheck";
     healthCheckRating: HealthCheckRating;
 }
@@ -34,7 +34,7 @@ interface Discharge {
     date: string;
     criteria: string;
 }
-interface HospitalEntry extends BaseEntry {
+export interface HospitalEntry extends BaseEntry {
     type: "Hospital";
     discharge: Discharge;
 }
@@ -43,7 +43,7 @@ interface SickLeave {
     startDate: string;
     endDate: string;
 }
-interface OccupationalHealthcareEntry extends BaseEntry {
+export interface OccupationalHealthcareEntry extends BaseEntry {
     type: "OccupationalHealthcare";
     employerName: string;
     sickLeave?: SickLeave;
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,18 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NewPatientEntry, Gender } from './types';
+import { isCaseOrDefaultClause } from 'typescript';
+import { NewPatientEntry,
+    Gender,
+    HealthCheckRating,
+    HealthCheckEntry,
+    BaseEntry,
+    HospitalEntry,
+    OccupationalHealthcareEntry,
+    Entry,
+    Discharge,
+    SickLeave,
+    DiagnosisEntry
+} from './types';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const toNewPatient = (object: any): NewPatientEntry => {
@@ -47,6 +59,116 @@ const parseGender = (gender: any): Gender => {
 
 const isGender = (param: any): param is Gender => {
     return Object.values(Gender).includes(param);
+};
+
+const parseHealthCheckRating = (rating: any): HealthCheckRating => {
+    if (!rating || !isRating(rating)) {
+        throw new Error('Incorrect or missing health check rating');
+    }
+    return rating;
+};
+
+const isRating = (param: any): param is HealthCheckRating => {
+    return Object.values(HealthCheckRating).includes(param);
+};
+
+const parseDiagnosisCodes = (codes: any): string[] => {
+    if (!codes || Array.isArray(codes) || isCode(codes)) {
+        throw new Error('Incorrect diagnosis codes');
+    }
+    return codes;
+};
+
+const isCode = (code: any): boolean => {
+    return code.every(i => (typeof i === "string"));
+};
+
+const parseDischarge = (discharge: any): Discharge => {
+    return {
+        date: parseDate(discharge.date),
+        criteria: parseString(discharge.criteria, "discharge criteria")
+    };
+};
+
+const parseSickLeave = (leave: any): SickLeave => {
+    return {
+        startDate: parseDate(leave.startDate),
+        endDate: parseDate(leave.endDate)
+    };
+};
+
+export const toNewEntry = (object: any): Entry => {
+    const assertNever = (value: never): never => {
+        throw new Error(
+          `Unhandled discriminated union member: ${JSON.stringify(value)}`
+        );
+      };
+      
+    switch (object.type) {
+        case "Hospital":
+            return parseHospitalEntry(object);
+        case "OccupationalHealthcare":
+            return parseOccupationalEntry(object);
+        case "HealthCheck":
+            return parseHealthEntry(object);
+        default:
+            return assertNever(object);
+    }
+};
+
+const parseBaseEntry = (object: any): BaseEntry => {
+    const baseEntry = {
+        id: parseString(object.id, "id"),
+        description: parseString(object.description, "description"),
+        date: parseDate(object.date),
+        specialist: parseString(object.specialist, "specialist"),
+    };
+    if (object.diagnosisCodes) {
+        return {
+            ...baseEntry,
+            diagnosisCodes: parseDiagnosisCodes(object.diagnosisCodes)
+        };
+    }
+    return baseEntry;
+};
+
+const parseHospitalEntry = (object: any): HospitalEntry => {
+    const newEntry = {
+        ...parseBaseEntry(object), 
+        type: "Hospital"
+    };
+    if (object.discharge) {
+        return {
+            ...newEntry,
+            discharge: parseDischarge(object.discharge)
+        };
+    }
+    return newEntry;
+};
+
+const parseOccupationalEntry = (object: any): OccupationalHealthcareEntry => {
+    const baseEntry = parseBaseEntry(object);
+    const newEntry = {
+        ...baseEntry,
+        type: "OccupationalHealthcare",
+        employerName: parseString(object.employerName, "employerName")
+    };
+    if (object.sickLeave) {
+        return {
+            ...newEntry,
+            sickLeave: parseSickLeave(object.sickLeave)
+        };
+    }
+    return newEntry;
+};
+
+const parseHealthEntry = (object: any): HealthCheckEntry => {
+    const baseEntry = parseBaseEntry(object);
+    return {
+        ...baseEntry,
+        type: "HealthCheck",
+        healthCheckRating: parseHealthCheckRating(object.healthCheckRating)
+    };
 };
 
 export default toNewPatient;
